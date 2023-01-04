a3b69682b6814b41a3609e7fe85362ec687e1de9
abdo <abdoAligmail.com>
2023-01-04 12:06:15 +0200

    Add Click Button

--- js/video-all.js
+++ js/video-all.js
@@ -2,61 +2,34 @@ import {header, navigation, getVideo} from "./global.js";
 header("../index.html","هيثم طلعت", "الرئيسية", "#visuals", "المرئيات", "../#bock", "الكتب","../#", "المقالات", "../html/indexEng.html", "English");
 navigation('../index.html', "الرئيسية", '../#visuals', "المرئيات", '../#bock', "الكتب", '../html/whit.html', 'من نحن', '../html/indexEng.html', "English");
 
-// // Videos Counte
-// let videos = document.querySelector(".videos");
-// getVideo(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCLj8UFOcdFrvlh24Lw7jrgA&maxResults=50&order=date&key=AIzaSyA6Bn7dJHlf7G8IxR7I_ZPCP_Ew-83sqkY`).then((result) => {
-//   for (let i = 0; i <= result.length; i++) {
-//     let box = document.createElement("div"); 
-//     box.className = "box"; 
-//     let createP = document.createElement("p");
-//     let repoNameAll = document.createTextNode(result[i].snippet.title);
-//     createP.appendChild(repoNameAll);
-//     box.appendChild(createP);
-//     videos.appendChild(box);
-//     box.innerHTML +=
-//       `<iframe
-//         width="95%"
-//         height="170"
-//         src="https://www.youtube.com/embed/${result[i].id.videoId}?rel=0"
-//         title="${result[i].snippet.title}" frameborder="0"allow="accelerometer;
-//         autoplay; clipboard-write; encrypted-media;
-//         gyroscope; picture-in-picture"allowfullscreen>
-//       </iframe>`;
-//   };
-// });
-
-// let viId = document.querySelectorAll(".visuals .box-all .box");
-// viId.forEach((ele) => {
-//   ele.addEventListener("click", (e) => {
-//     videos.innerHTML = "";
-//     viId.forEach(e => e.classList.remove("active"));
-//     e.currentTarget.classList.add("active");
-//     getVideo(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${ele.id}&maxResults=50&order=date&key=AIzaSyA6Bn7dJHlf7G8IxR7I_ZPCP_Ew-83sqkY`).then((result) => {
-//       for (let i = 0; i <= result.length; i++) {
-//         let box = document.createElement("div"); 
-//         box.className = "box";
-//         let createP = document.createElement("p");
-//         let repoNameAll = document.createTextNode(result[i].snippet.title);
-//         createP.appendChild(repoNameAll);
-//         box.appendChild(createP);
-//         videos.appendChild(box);
-//         box.innerHTML +=
-//           `<iframe
-//             width="95%"
-//             height="170"
-//             src="https://www.youtube.com/embed/${result[i].id.videoId}?rel=0"
-//             title="${result[i].snippet.title}" frameborder="0"allow="accelerometer;
-//             autoplay; clipboard-write; encrypted-media;
-//             gyroscope; picture-in-picture"allowfullscreen>
-//           </iframe>`;
-//       };
-//     });
-//   });
-// });
-
-
-
-
+let viId = document.querySelectorAll(".visuals .box-all .box");
+viId.forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    videos.innerHTML = "";
+    viId.forEach(e => e.classList.remove("active"));
+    e.currentTarget.classList.add("active");
+    getVideo(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${ele.id}&maxResults=50&order=date&key=AIzaSyA6Bn7dJHlf7G8IxR7I_ZPCP_Ew-83sqkY`).then((result) => {
+      for (let i = 0; i <= result.length; i++) {
+        let box = document.createElement("div"); 
+        box.className = "box";
+        let createP = document.createElement("p");
+        let repoNameAll = document.createTextNode(result[i].snippet.title);
+        createP.appendChild(repoNameAll);
+        box.appendChild(createP);
+        videos.appendChild(box);
+        box.innerHTML +=
+          `<iframe
+            width="95%"
+            height="170"
+            src="https://www.youtube.com/embed/${result[i].id.videoId}?rel=0"
+            title="${result[i].snippet.title}" frameborder="0"allow="accelerometer;
+            autoplay; clipboard-write; encrypted-media;
+            gyroscope; picture-in-picture"allowfullscreen>
+          </iframe>`;
+      };
+    });
+  });
+});
 
 
 // Videos Counte
@@ -83,24 +56,36 @@ getVideo(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelI
       }
   }
 
-const generator = generateSomething();
+// Start Click In Genarators videos
+let buttonChild = document.querySelectorAll(".clickButton div");
 
+const generator = generateSomething();
+  // Videos 10 Number
   for (let i = 0; i <= 9; i++) {
     generator.next().value;
   }
-  
-  document.getElementById('click').addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
+  buttonChild.forEach((a) => {
+    a.addEventListener("click", (e) => {
+      // Remove Class Active
+      buttonChild.forEach((a) => a.classList.remove("active"));
+      // Add Class Active
+      e.currentTarget.classList.add("active");
+      // Add Events No Click
+      a.style.cssText = "pointer-events: none";
+      videos.innerHTML = "";
+      // Scrolling To Top
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+      // Add Videos On Click 10 Number
+      for (let i = 0; i <= 9; i++) {
+        generator.next().value;
+      }
     });
-    videos.innerHTML = "";
-    for (let i = 0; i <= 9; i++) {
-      generator.next().value;
-    }
   });
 });
-
+// End Click In Genarators videos
 
  
